2d2f1aa281dcf62224d3a7a273ca93e2923c07ff
danniel917 <danniel917@hotmail.com>
2024-02-22 21:18:07 -0500

    Create file 'PathLinks.ks' to store path links for the menu.

---
+++ src/Components/NavBar/PathLinks.js
@@ -0,0 +1,56 @@
+const left_items = [
+    {
+      path_name: "Shopi",
+      path_link: "/",
+      class_Name: "font-semibold text-lg"
+    },
+    {
+      path_name: "All",
+      path_link: "/",
+    },
+    {
+      path_name: "Clothes",
+      path_link: "/clothes",
+    },
+    {
+      path_name: "Electronics",
+      path_link: "/electronics",
+    },
+    {
+      path_name: "Furnitures",
+      path_link: "/furnitures",
+    },
+    {
+      path_name: "Toys",
+      path_link: "/toys",
+    },
+    {
+      path_name: "Others",
+      path_link: "/others",
+    },
+  ];
+
+  const right_items = [
+    {
+      path_name: "noreply@hotmail.com",
+      path_link: ""
+    },
+    {
+      path_name: "My Orders",
+      path_link: "/my-orders",
+    },
+    {
+      path_name: "My Account",
+      path_link: "/my-account",
+    },
+    {
+      path_name: "Sign In",
+      path_link: "/sign-in",
+    },
+    {
+      path_name: "Cart 0",
+      path_link: "",
+    },
+  ];
+
+  export {left_items, right_items}
